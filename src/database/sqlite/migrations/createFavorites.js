6b424193d933df9b57b46acf6b6e7cf7f03bf477
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-06-10 17:52:34 -0300

    Added cascade in ingredients and favorites tables

--- src/database/sqlite/migrations/createFavorites.js
+++ src/database/sqlite/migrations/createFavorites.js
@@ -2,7 +2,7 @@ const createFavorites = `
     CREATE TABLE IF NOT EXISTS favorites (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER NOT NULL REFERENCES users (id),
-        dish_id INTEGER NOT NULL REFERENCES dishes (id)
+        dish_id INTEGER NOT NULL REFERENCES dishes (id) ON DELETE CASCADE
     );
 `;
 
--- src/database/sqlite/migrations/createIngredients.js
+++ src/database/sqlite/migrations/createIngredients.js
@@ -1,7 +1,7 @@
 const createIngredients = `
     CREATE TABLE IF NOT EXISTS ingredients (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        dish_id INTEGER NOT NULL REFERENCES dishes (id),
+        dish_id INTEGER NOT NULL REFERENCES dishes (id) ON DELETE CASCADE,
         name VARCHAR NOT NULL
     );
 `;
