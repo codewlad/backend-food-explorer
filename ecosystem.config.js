4573a319ef9839416b87d99ec5f70049a3baee4c
Wladimir Ferreira Gonçalves <codewlad@gmail.com>
2023-07-29 14:06:35 -0300

    Added deployment settings

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: "./src/server.js",
+    instances: 1,
+    //instances: "max",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
